d22d93dd71b69b139ca41d4d3ce6665fc0e4673d
tomassirio <tomassirio@gmail.com>
2025-10-24 11:02:06 +0200

    Move initial_screen to screens

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:tracker_frontend/presentation/screens/home_screen.dart';
-import 'package:tracker_frontend/data/storage/token_storage.dart';
+import 'package:tracker_frontend/presentation/screens/initial_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -20,77 +19,5 @@ class MyApp extends StatelessWidget {
       ),
       home: const InitialScreen(),
     );
-  }
-}
-
-/// Initial screen that checks auth state and shows appropriate content
-class InitialScreen extends StatefulWidget {
-  const InitialScreen({super.key});
-
-  @override
-  State<InitialScreen> createState() => _InitialScreenState();
-}
-
-class _InitialScreenState extends State<InitialScreen> {
-  final TokenStorage _tokenStorage = TokenStorage();
-  bool _isChecking = true;
-
-  @override
-  void initState() {
-    super.initState();
-    _checkAuthState();
-  }
-
-  Future<void> _checkAuthState() async {
-    // Small delay to show splash-like effect
-    await Future.delayed(const Duration(milliseconds: 500));
-
-    // Check if user is logged in
-    final isLoggedIn = await _tokenStorage.isLoggedIn();
-
-    if (mounted) {
-      setState(() {
-        _isChecking = false;
-      });
-    }
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    if (_isChecking) {
-      return Scaffold(
-        body: Container(
-          decoration: BoxDecoration(
-            gradient: LinearGradient(
-              begin: Alignment.topLeft,
-              end: Alignment.bottomRight,
-              colors: [
-                Theme.of(context).colorScheme.primary,
-                Theme.of(context).colorScheme.secondary,
-              ],
-            ),
-          ),
-          child: const Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Icon(
-                  Icons.explore,
-                  size: 80,
-                  color: Colors.white,
-                ),
-                SizedBox(height: 24),
-                CircularProgressIndicator(
-                  valueColor: AlwaysStoppedAnimation<Color>(Colors.white),
-                ),
-              ],
-            ),
-          ),
-        ),
-      );
-    }
-
-    // Always show HomeScreen - it will handle showing public trips or user's trips
-    return const HomeScreen();
   }
 }
---
+++ lib/presentation/screens/initial_screen.dart
@@ -0,0 +1,70 @@
+import 'package:flutter/material.dart';
+import 'package:tracker_frontend/presentation/screens/home_screen.dart';
+
+/// Initial screen that checks auth state and shows appropriate content
+class InitialScreen extends StatefulWidget {
+  const InitialScreen({super.key});
+
+  @override
+  State<InitialScreen> createState() => _InitialScreenState();
+}
+
+class _InitialScreenState extends State<InitialScreen> {
+  bool _isChecking = true;
+
+  @override
+  void initState() {
+    super.initState();
+    _checkAuthState();
+  }
+
+  Future<void> _checkAuthState() async {
+    // Small delay to show splash-like effect
+    await Future.delayed(const Duration(milliseconds: 500));
+
+    if (mounted) {
+      setState(() {
+        _isChecking = false;
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    if (_isChecking) {
+      return Scaffold(
+        body: Container(
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+              colors: [
+                Theme.of(context).colorScheme.primary,
+                Theme.of(context).colorScheme.secondary,
+              ],
+            ),
+          ),
+          child: const Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Icon(
+                  Icons.explore,
+                  size: 80,
+                  color: Colors.white,
+                ),
+                SizedBox(height: 24),
+                CircularProgressIndicator(
+                  valueColor: AlwaysStoppedAnimation<Color>(Colors.white),
+                ),
+              ],
+            ),
+          ),
+        ),
+      );
+    }
+
+    // Always show HomeScreen - it will handle showing public trips or user's trips
+    return const HomeScreen();
+  }
+}
