1533bb9c673948a595b721fd6ff0cdbc4ee8e23a
tomassirio <tomassirio@gmail.com>
2025-10-28 13:12:15 +0100

    Fix size of cards

--- lib/presentation/widgets/home/youtube_home_content.dart
+++ lib/presentation/widgets/home/youtube_home_content.dart
@@ -156,8 +156,7 @@ class YouTubeHomeContent extends StatelessWidget {
       physics: const NeverScrollableScrollPhysics(),
       gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
         crossAxisCount: crossAxisCount,
-        childAspectRatio:
-            0.85, // Slightly taller for smaller cards with 4:3 ratio
+        childAspectRatio: 1,
         crossAxisSpacing: 16,
         mainAxisSpacing: 16,
       ),
